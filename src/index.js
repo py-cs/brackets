b9a7f48b30f8cbb643cb3fcee2b1f20f3f704d1c
cs-py <e.1@mail.ru>
2021-02-28 17:11:30 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.map(el => el.join(''))
+  do {
+    let size = str.length
+    brackets.forEach(el => str = str.replace(el, ''))
+    changed = str.length !== size
+  } while (changed)
+  return !Boolean(str)
 }
